3bee56584d746d47c729fb2ae53269d2d8b1eefa
Gitam Gadtaula <gitamgadtaula@gmail.com>
2024-03-27 09:13:45 +0545

    production ready 8.0 version demo

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template>
-  <h1 class="text-4xl p-4 text-center">Vue3-Radio-Card Demo</h1>
-  <div class="light mb-4">
-    <!-- component -->
-    <p class="text-sm text-center">Example with item slot</p>
-    <radio-card v-model="selected" :options="items" title="Light mode">
-      <template #item="{ value, price }">
-        <span class="font-bold text-gray-900">
-          <span class="text-4xl">{{ value }}</span>
-        </span>
-        <span>
-          <span class="text-xl font-bold text-gray-500">$</span>
-          <span class="text-xl font-bold text-gray-900 -ml-1"
-            >&nbsp;{{ price }}</span
-          >
-          <span class="text-xl font-semibold text-gray-500">/</span>
-          <span class="text-lg font-semibold text-gray-500">mo</span>
-        </span>
-      </template>
-    </radio-card>
-  </div>
-  <div class="dark mt-4">
-    <!-- component -->
-    <p class="text-sm text-center">Example without slot</p>
+  <section class="p-4">
+    <h1 class="text-4xl p-4 text-center">Vue3-Radio-Card Demo</h1>
+    <div class="light mb-4">
+      <!-- component -->
+      <p class="text-sm text-center">Example with item slot</p>
+      <radio-card v-model="selected" :options="items" title="Light mode">
+        <template #item="{ value, price }">
+          <span class="font-bold text-gray-900">
+            <span class="text-4xl">{{ value }}</span>
+          </span>
+          <span>
+            <span class="text-xl font-bold text-gray-500">$</span>
+            <span class="text-xl font-bold text-gray-900 -ml-1"
+              >&nbsp;{{ price }}</span
+            >
+            <span class="text-xl font-semibold text-gray-500">/</span>
+            <span class="text-lg font-semibold text-gray-500">mo</span>
+          </span>
+        </template>
+      </radio-card>
+    </div>
+    <div class="dark mt-4">
+      <!-- component -->
+      <p class="text-sm text-center">Example without slot</p>
 
-    <radio-card v-model="selected" :options="items" title="Dark mode">
-    </radio-card>
-  </div>
+      <radio-card v-model="selected" :options="items" title="Dark mode">
+      </radio-card>
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
