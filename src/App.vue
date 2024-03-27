33cba953293b27e203a2e8dfea0a06a1ea6b58d1
Gitam Gadtaula <gitamgadtaula@gmail.com>
2024-03-27 09:06:10 +0545

    added demo for dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <h1 class="text-4xl p-4 text-center">Vue3-Radio-Card Demo</h1>
+  <div class="light mb-4">
     <!-- component -->
-
-    <radio-card v-model="selected" :options="items" title="Choose your plan">
+    <p class="text-sm text-center">Example with item slot</p>
+    <radio-card v-model="selected" :options="items" title="Light mode">
       <template #item="{ value, price }">
         <span class="font-bold text-gray-900">
           <span class="text-4xl">{{ value }}</span>
-          <span class="text-2xl uppercase">GB</span>
         </span>
         <span>
           <span class="text-xl font-bold text-gray-500">$</span>
@@ -19,6 +19,13 @@
       </template>
     </radio-card>
   </div>
+  <div class="dark mt-4">
+    <!-- component -->
+    <p class="text-sm text-center">Example without slot</p>
+
+    <radio-card v-model="selected" :options="items" title="Dark mode">
+    </radio-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -26,11 +33,12 @@ import { ref } from "vue";
 import { RadioCard } from "vue3-radio-card";
 import "vue3-radio-card/style.css";
 const selected = ref();
+
 const items = [
-  { label: "50", value: "50", price: 5 },
-  { label: "80", value: "80", price: 7 },
-  { label: "100", value: "100", price: 10 },
-  { label: "200", value: "200", price: 18 },
+  { label: "50 GB", value: "50", price: 5 },
+  { label: "80 GB", value: "80", price: 7 },
+  { label: "100 GB", value: "100", price: 10 },
+  { label: "200 GB", value: "200", price: 18 },
 ];
 </script>
 
